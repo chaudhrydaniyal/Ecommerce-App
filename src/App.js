7b80c71ba23586256e0443a5aedfe6c5d1dc4e28
Daniyal khalid <daniyalkhalid@yahoo.com>
2021-07-13 23:54:50 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Homepage from './components/homepage';
+import './App.css';
+
+function App() {
+  return (
+    <div class="App">
+      
+      <Homepage/>
+      
+    </div>
+  );
+}
+
+export default App;
